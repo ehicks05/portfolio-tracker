87c952c1c01caa771ece1fbe39098dd376aaab73
Eric Hicks <ehicks05@yahoo.com>
2021-09-22 23:05:30 -0400

    tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
+    <div className="">
       <header className="App-header">
         <p>Hello Vite + React!</p>
         <p>
@@ -12,29 +12,8 @@ function App() {
             count is: {count}
           </button>
         </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
         <p>sup my dude</p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        <p></p>
       </header>
     </div>
   );
