349dd8922a3d80c827474d64d92ae273d7f47370
Eric Hicks <ehicks05@yahoo.com>
2021-10-13 21:34:10 -0400

    update tax logic

--- src/tax.ts
+++ src/tax.ts
@@ -1,7 +1,7 @@
 const FEDERAL_DEDUCTION = 12550;
 const STATE_DEDUCTION = 1000;
 
-const federal = [
+const FED_SCHEDULE = [
   [0, 9950, 0.1],
   [9951, 40525, 0.12],
   [40526, 86375, 0.22],
@@ -11,14 +11,13 @@ const federal = [
   [523601, 900_000_000, 0.37],
 ];
 
-// Up to $40,400	$40,401 – $445,850	Over $445,850
-const longTerm = [
+const LONG_TERM_SCHEDULE = [
   [0, 40400, 0.0],
   [40401, 445850, 0.15],
   [445851, 900_000_000, 0.2],
 ];
 
-const state = [
+const STATE_SCHEDULE = [
   [0, 20000, 0.014],
   [20001, 35000, 0.0175],
   [35001, 40000, 0.035],
@@ -41,19 +40,68 @@ const calculateTax = (income: number, schedule: any) => {
   return totalTaxes;
 };
 
-const calculateStateTax = (income: number) =>
-  calculateTax(income - STATE_DEDUCTION, state);
-const calculateFederalTax = (income: number) =>
-  calculateTax(income - FEDERAL_DEDUCTION, federal);
-const calculateLongTermTax = (income: number) =>
-  calculateTax(income - FEDERAL_DEDUCTION, longTerm);
-const calculateCombinedTax = (income: number) =>
-  calculateFederalTax(income) + calculateStateTax(income);
+const calculateTaxes = ({
+  shortTerm,
+  longTerm,
+}: {
+  shortTerm: number;
+  longTerm: number;
+}) => {
+  const income = shortTerm + longTerm;
+  // state
+  const stateTax = calculateTax(income - STATE_DEDUCTION, STATE_SCHEDULE);
+  const stateTaxRate = stateTax / income;
 
-export {
-  FEDERAL_DEDUCTION,
-  calculateCombinedTax,
-  calculateFederalTax,
-  calculateLongTermTax,
-  calculateStateTax,
+  // federal
+  const shortTermFederalIncome =
+    shortTerm >= FEDERAL_DEDUCTION ? shortTerm - FEDERAL_DEDUCTION : 0;
+  let remainingDeduction =
+    shortTerm >= FEDERAL_DEDUCTION ? 0 : FEDERAL_DEDUCTION - shortTerm;
+
+  const longTermFederalIncome =
+    longTerm >= remainingDeduction ? longTerm - remainingDeduction : 0;
+
+  const shortTermFedTax = calculateTax(shortTermFederalIncome, FED_SCHEDULE);
+  const shortTermFedTaxRate = shortTermFedTax / shortTermFederalIncome;
+
+  const adjustedLongTermSchedule = LONG_TERM_SCHEDULE.map((row) => [
+    row[0] - shortTermFederalIncome,
+    row[1] - shortTermFederalIncome,
+    row[2],
+  ]).filter((row) => row[1] !== 0);
+
+  const longTermFedTax = calculateTax(
+    longTermFederalIncome,
+    adjustedLongTermSchedule
+  );
+  const longTermFedTaxRate = longTermFedTax / longTermFederalIncome;
+
+  const federalTax = shortTermFedTax + longTermFedTax;
+  const federalTaxRate = (shortTermFedTax + longTermFedTax) / income;
+
+  const niit = longTerm > 200_000 ? (longTerm - 200_000) * 0.038 : 0;
+  const niitRate = niit / longTerm;
+
+  const overallTax = stateTax + federalTax + niit;
+  const overallTaxRate = overallTax / income;
+
+  const remaining = income - stateTax - shortTermFedTax - longTermFedTax - niit;
+
+  return {
+    stateTax,
+    stateTaxRate,
+    shortTermFedTax,
+    shortTermFedTaxRate,
+    longTermFedTax,
+    longTermFedTaxRate,
+    federalTax,
+    federalTaxRate,
+    overallTax,
+    overallTaxRate,
+    niit,
+    niitRate,
+    remaining,
+  };
 };
+
+export { calculateTaxes };
